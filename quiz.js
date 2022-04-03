1b9895042a0cf2e5b7c8804752a893d24befe152
Alex Leong <alex@buoyant.io>
2022-04-02 21:42:06 -0700

    Revert "April Fools"
    This reverts commit 6a4856ae4ab7157caea773bec342956191964dfe.

--- quiz.js
+++ quiz.js
@@ -1,4 +1,4 @@
-const awesome = require("./awesome");
+
 const money = require("./money");
 
 const re = /(.*)[-~]\W*(\w+)/;
@@ -21,8 +21,6 @@ async function command(chatClient, apiClient, channel, db) {
         chatClient.say(channel, `No quizzes while ${channel} isn't live.`);
         return;
     }
-
-    console.log(db);
     
     const { rows } = await db.query('SELECT message, game, quoted_by FROM quotes WHERE CHANNEL = $1 ORDER BY random() LIMIT 1', [channel]);
     const match = rows[0].message.match(re);
@@ -35,7 +33,7 @@ async function command(chatClient, apiClient, channel, db) {
     chatClient.say(channel, match[1]);
     chatClient.say(channel, "(You have 60 seconds to answer starting... NOW)");
     quiz = {
-        correct: awesome.get(channel).toLowerCase(),
+        correct: match[2].toLowerCase(),
         answers: {}
     }
     setTimeout(() => {
@@ -70,21 +68,13 @@ function endRound1(chatClient, channel, db) {
     });
 }
 
-function game() {
-    const games = ["Grand Theft Auto V", "VALORANT", "Fortnite", "League of Legends", "Apex Legends", "Elden Ring",
-        "Overwatch", "Dota 2", "Counter-Strike: Global Offensive",
-        "Call of Duty: Modern Warfare", "ASMR", "Pools, Hot Tubs, and Beaches" ];
-    return games[Math.floor(Math.random() * games.length)];
-}
-
-
 async function startRound2(chatClient, channel, db) {
     const { rows } = await db.query('SELECT message, game, quoted_by FROM quotes WHERE CHANNEL = $1 ORDER BY random() LIMIT 1', [channel]);
     chatClient.say(channel, "ROUND 2: What GAME is this quote from?");
     chatClient.say(channel, rows[0].message);
     chatClient.say(channel, "(You have 60 seconds to answer starting... NOW)");
     quiz = {
-        correct: game(),
+        correct: rows[0].game.toLowerCase(),
         answers: {}
     }
     setTimeout(() => {
@@ -131,7 +121,7 @@ async function startRound3(chatClient, channel, db) {
     chatClient.say(channel, words.join(' '));
     chatClient.say(channel, "(You have 60 seconds to answer starting... NOW)");
     quiz = {
-        correct: "metapod",
+        correct: word.toLowerCase(),
         answers: {}
     }
     setTimeout(() => {
