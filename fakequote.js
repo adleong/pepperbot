7668602907555091c3c109a1fad9bf6000f24094
Alex Leong <alex@buoyant.io>
2022-06-01 20:45:52 -0700

    Add safety content filtering
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- fakequote.js
+++ fakequote.js
@@ -15,6 +15,28 @@ async function command(chatClient, channel) {
 }
 
 async function fake() {
+    const quote = await create();
+    const response = await openai.createCompletion("content-filter-alpha",{
+        prompt: "<|endoftext|>"+quote+"\n--\nLabel:",
+        temperature: 0,
+        max_tokens: 1,
+        top_p: 0,
+        logprobs: 10
+    })
+    const classification = response.data.choices[0];
+    if (classification.text == "0" || classification.text == "1") {
+        return quote;
+    }
+    if (classification.logprobs.top_logprobs[0][classification.text] < -0.355) {
+        return quote;
+    }
+    console.log("Rejecting unsafe quote: " + quote);
+    console.log(classification.text);
+    console.log(classification.logprobs.top_logprobs[0]);
+    return await fake();
+}
+
+async function create() {
     const response = await openai.createCompletionFromModel({
         model: "curie:ft-personal-2022-04-29-00-17-58",
         prompt: "",
