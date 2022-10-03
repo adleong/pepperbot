01f1a032b13d7f6601cdf5cff30f8dc3b3e6e8c0
Alex Leong <alex@buoyant.io>
2022-10-02 18:51:26 -0700

    Add keepalive to say
    Signed-off-by: Alex Leong <alex@buoyant.io>

--- say.js
+++ say.js
@@ -1,19 +1,28 @@
-let listeners = [];
+let listener = null;
 
 function listen(res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Transfer-Encoding', 'chunked');
     res.setHeader('Connection', 'keep-alive');
-    listeners[0] = res;
+    listener = res;
 }
 
 function say(msg) {
-    listeners.forEach(function (res) {
-        res.write(JSON.stringify(msg));
-        res.write("\n");
-    });
+    if (listener) {
+        listener.write(JSON.stringify({"say": msg}));
+        listener.write("\n");
+    };
 }
+
+// Keep the connection alive
+setInterval(() => {
+    if (listener) {
+        listener.write(JSON.stringify({}));
+        listener.write("\n");
+    };
+}, 20000);
+
 
 module.exports = {
     listen,
